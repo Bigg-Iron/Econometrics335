b76fc2627e30f781d8ae29061864549997fed754
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-08-31 00:39:12 -0600

    Added Bernoulli Trials

--- econometrics-with.R
+++ econometrics-with.R
@@ -17,9 +17,22 @@ plot(probability,
 # generate the vector of cumulative probabilities 
 cum_probability <- cumsum(probability) 
 
-# plot the probabilites 
+# plot the probabilities 
 plot(cum_probability, 
      xlab = "outcomes", 
      main = "Cumulative Probability Distribution") 
 
+# Vectors
+y <- c(1,2,3,4,5)
 
+# Functions:
+# generate the vector `z`
+z <- seq(from = 1, to = 5, by = 1)
+
+# compute the mean of the entries in `z`
+mean(z)
+
+# Bernoulli Trials
+# The set of elements from which sample() draws outcomes does not have to consist of numbers only. We might as well simulate coin tossing with outcomes  
+# H (heads) and T (tails)
+coin_toss <- sample(c("H", "T"), 1) 
