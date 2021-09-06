aabf7bfa3a5ef02e1e2fc44476c9540b596d680a
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-09-06 15:20:12 -0600

    Formatting

--- Econ335ClassA-1.R
+++ Econ335ClassA-1.R
@@ -1,84 +1,85 @@
 
-#Basics
-1+1
+# Basics
+1 + 1
 
-#create a variable that is equal to 5:
-x<-5
+# create a variable that is equal to 5:
+x <- 5
 
-y<-10
+y <- 10
 
-#Variables are assigned using “<-”, although “=” alsoworks.
+# Variables are assigned using “<-”, although “=” alsoworks.
 
-x+y
+x + y
 
-z<-x+y 
+z <- x + y
 
 print(z)
 
-#Note that you can use other operations as well such as addition, subtraction, multiplication, division, etc
-#Note: Variables in R are defined as vectors. So far x, y, and z are all vectors of 1. 
+# Note that you can use other operations as well such as addition, subtraction, multiplication, division, etc
+# Note: Variables in R are defined as vectors. So far x, y, and z are all vectors of 1.
 
-w<-c(1,2,3,4,5,6,7,8,9) #vector
+w <- c(1, 2, 3, 4, 5, 6, 7, 8, 9) # vector
 
 mean(w)
-mean(1:9)# compute mean of natural numbers from 1 to 9
+mean(1:9) # compute mean of natural numbers from 1 to 9
 median(w)
 sum(w)
 
-#Some Visualizations
-#Recall Chapter2 Roll a dice
-rolldie<-sample(1:6,1)
+# Some Visualizations
+# Recall Chapter2 Roll a dice
+rolldie <- sample(1:6, 1)
 
 # generate the vector of probabilities
-probability <- rep(1/6, 6)
+probability <- rep(1 / 6, 6)
 # plot the probabilities
 plot(probability,
      xlab = "outcomes",
-     main = "Probability Distribution")
+     main = "Probability Distribution"
+)
 
 # generate the vector of cumulative probabilities
 cum_probability <- cumsum(probability)
 # plot the probabilities
 plot(cum_probability,
      xlab = "outcomes",
-     main = "Cumulative Probability Distribution")
+     main = "Cumulative Probability Distribution"
+)
 
-#we can conveniently obtain densities of normal distributions using the function dnorm(). i.e., draw a plot of the N(0,1) 
+# we can conveniently obtain densities of normal distributions using the function dnorm(). i.e., draw a plot of the N(0,1)
 curve(dnorm(x),
-      xlim = c(-2.5, 2.5),
-      ylab = "Density",
-      main = "Standard Normal Density Function")
+     xlim = c(-2.5, 2.5),
+     ylab = "Density",
+     main = "Standard Normal Density Function"
+)
 
-#So far we're in Base R. Since changing the language is complicated, most innovation is done through PACKAGES. 
-#install.packages("tidyverse")
-#Note: This package has many useful features which we will be using throughout the course. 
+# So far we're in Base R. Since changing the language is complicated, most innovation is done through PACKAGES. # nolint
+# install.packages("tidyverse")
+# Note: This package has many useful features which we will be using throughout the course.
 
-#call the package using:
+# call the package using:
 library("tidyverse")
 
 # Required for skewness()and kurtosis() functions
 install.packages("moments")
 library(moments)
 
-#Now lets load our California school district example from Stock & Watson
-caschool1 <- read.csv('data/caschool.csv')
+# Now lets load our California school district example from Stock & Watson
+caschool1 <- read.csv("data/caschool.csv")
 
-#Inspect the data
-head(caschool1) #look at first 6 rows (all columns)
-str(caschool1) #structure # rows, cols, data types
-nrow(caschool1) #number of rows
-ncol(caschool1) #number of columns
-names(caschool1) #column names
+# Inspect the data
+head(caschool1) # look at first 6 rows (all columns)
+str(caschool1) # structure # rows, cols, data types
+nrow(caschool1) # number of rows
+ncol(caschool1) # number of columns
+names(caschool1) # column names
 
-#Statistics
-summary(caschool1) #does summary stats for each column 
-mean_tscore<-summarize(caschool1, mean(testscr))
+# Statistics
+summary(caschool1) # does summary stats for each column
+mean_tscore <- summarize(caschool1, mean(testscr))
 
-#Or
-summarize(caschool1, mean_tscore=mean(testscr), sd_tscore=sd(testscr), sk_tscore=skewness(testscr), k_tscore=kurtosis(testscr), n())
+# Or
+summarize(caschool1, mean_tscore = mean(testscr), sd_tscore = sd(testscr), sk_tscore = skewness(testscr), k_tscore = kurtosis(testscr), n())
 
-#ggplot2 is a data visualization package of R that provides a general scheme for data visualization.
-ggplot(data = caschool1) +  
-  geom_point(mapping = aes(x = str, y = testscr), color="green")
-  
-
+# ggplot2 is a data visualization package of R that provides a general scheme for data visualization.
+ggplot(data = caschool1) +
+     geom_point(mapping = aes(x = str, y = testscr), color = "green")
