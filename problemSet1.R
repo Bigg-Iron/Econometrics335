70a6f0a93497892a26cb75db688bed44a26b5a11
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-09-07 11:05:15 -0600

    Added more questions for problem set 1

--- problemSet1.R
+++ problemSet1.R
@@ -27,3 +27,7 @@ print(mean_per_capita_inc)
 # 4.
 #   a)
 #     i) Sample mean
+#     ii) Sample Standard Deviation
+#     iii) Sample skewness
+#     iv) Sample kurtosis and (v)
+#     v) Sample size for per capita income (variable name is "PerCapitaInc")
