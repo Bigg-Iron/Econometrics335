0af0ec0b272986cfb22e54ce9bd249ca87dd1854
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-09-08 14:56:08 -0600

    Solved sample_size

--- problemSets/problemSet1.R
+++ problemSets/problemSet1.R
@@ -21,25 +21,30 @@ summary(df)
 my_data <- drop_na(df)
 summary(my_data)
 
-my_data <- as_tibble(df)
-
-# Summarize data
-sample_stats <- summarise(my_data, across(everything(), list(mean)))
+# Summarize PerCapitaInc for sample statistics
+summarize(df, mean=mean(PerCapitaInc), sd=sd(PerCapitaInc), skewness=skewness(PerCapitaInc), kurtosis=kurtosis(PerCapitaInc), n())
 
 
 # 4.
 #   a)
 #     i) Sample mean PerCapitaInc
-sample_mean_PerCapitaInc <- summarise(my_data, mean = mean(PerCapitaInc))
+mean(df$PerCapitaInc, na.rm=TRUE)
 
 #     ii) Sample Standard Deviation PerCapitaInc
-sample_standard_deviation <- summarise(my_data, s = mean(PerCapitaInc))
+sd(df$PerCapitaInc, na.rm=TRUE)
 
 #     iii) Sample skewness PerCapitaInc (package: moments)
-
+skewness(df$PerCapitaInc, na.rm=TRUE)
 
 #     iv) Sample kurtosis and (v) PerCapitaInc (package: moments)
-
+kurtosis(df$PerCapitaInc, na.rm=TRUE)
 
 #     v) Sample size for per capita income (variable name is "PerCapitaInc")
+sample_size <- summarize(df, n())
 
+# Min
+min(df$PerCapitaInc, na.rm=TRUE)
+# Max
+max(df$PerCapitaInc, na.rm=TRUE)
+
+# Indicate in your answer whether the per capita income distribution has a longer right tail or a longer left tail (and how you know). Also indicate whether the tails of the distribution would be considered statistically “heavy” or not (and how you know).
