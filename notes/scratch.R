28c3684958c6499f84d0b148c1ded01f58402678
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-09-26 12:19:09 -0600

    Added data retrieval example for Poudre river flow

--- notes/scratch.R
+++ notes/scratch.R
@@ -1,4 +1,24 @@
-install.packages("esquisse")
-library(esquisse)
+# Data retrieval 
+install.packages("dataRetrieval")
 
-esquisse::esquisser()
+library(dataRetrieval)
+
+# Retrieve flow data for Poudre River
+poudre <- readNWISdv(siteNumbers = '06752260',
+                     parameterCd = c('00060'),
+                     startDate = '2010-10-01',
+                     endDate = Sys.Date())
+
+head(poudre)
+
+#Remove the first two columns
+pq <- poudre[,-c(1,2,5)]
+
+#Rename the remaining columns
+names(pq) <- c('Date','q_cfs')
+
+summary(pq)
+
+# Plot the data
+plot(pq$q_cfs ~ pq$Date, type = 'l', ylab = 'River Flow (cfs)', 
+     xlab = 'Date',col = 'blue3')
