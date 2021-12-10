c5a1ee2c4639283851c78d4f5088a567f88d2c2d
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-12-09 17:09:51 -0700

    Adjusted income models

--- FINAL_PROJECT/scratch.R
+++ FINAL_PROJECT/scratch.R
@@ -1,13 +1,26 @@
 summary(data_table_16)
 
 
-model_1 <- lm(data_table_16$age_at_d ~ data_table_16$indv_inc)
-summary(model_1)
+# subset the data (remove income values less than $1)
+adjusted_income <- subset(data_table_16, indv_inc > 1)
 
-plot(x = data_table_16$indv_inc, 
-     y = data_table_16$age_at_d, 
-     xlab = "Individual Income",
-     ylab = "Age at Death",
+
+
+# estimate simple regression models
+adjusted_income_model <- lm(age_at_d ~ indv_inc, data = adjusted_income)
+summary(adjusted_income_model)
+coeftest(adjusted_income_model, vcov. = vcovHC, type = "HC1")
+
+
+
+
+# plot the observations and add the estimated regression line
+# Adjusted income model (Income above $1)
+plot(x = adjusted_income$indv_inc, 
+     y = adjusted_income$age_at_d, 
+     xlab = "Income",
+     ylab = "Age",
      main = "Income and Life Expectancy",
-     col = "steelblue",
-abline(model_1))
+     col = "steelblue")
+abline(adjusted_income_model, col = "red")
+
