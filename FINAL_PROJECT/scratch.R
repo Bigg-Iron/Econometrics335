096f4f4b425dd828a919b673e3096fc60c7478bd
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-12-12 18:17:31 -0700

    Stargazer

--- FINAL_PROJECT/scratch.R
+++ FINAL_PROJECT/scratch.R
@@ -1,5 +1,27 @@
+# Code
+sink(file = "data_structure_summary.txt")
+
+cat("Summary statistics for table 16")
+cat("\n-------------------------------------------------\n")
+
+str(data_table_16)
+cat("\n")
 summary(data_table_16)
 
+
+sink()
+
+
+
+## Graphs and Figures
+# Plot Income and Mortality 
+plot(x = data_table_16$indv_inc, 
+     y = data_table_16$mortrate, 
+     xlab = "Income",
+     ylab = "Mortality Rate",
+     main = "Mortality Rates and Income",
+     col = "steelblue")
+abline(mortality_model, col = "red")
 
 
 # estimate simple regression models
@@ -53,3 +75,20 @@ plot(x = data_table_16$age_at_d,
      main = "Mortality Rates and Age",
      col = "steelblue")
 abline(age_mortality_model, col = "red")
+
+
+
+
+
+# generate the table
+stargazer(fatalities_mod1, fatalities_mod2, fatalities_mod3, 
+          fatalities_mod4, fatalities_mod5, fatalities_mod6, fatalities_mod7, 
+          digits = 3,
+          header = FALSE,
+          type = "latex", 
+          se = rob_se,
+          title = "Linear Panel Regression Models of Traffic Fatalities due to Drunk Driving",
+          model.numbers = FALSE,
+          column.labels = c("(1)", "(2)", "(3)", "(4)", "(5)", "(6)", "(7)"),
+          out="table10.txt")
+
