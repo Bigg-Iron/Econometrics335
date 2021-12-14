0aa05ccec20c55b93fb239f3c60336a9594eda7d
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-12-14 13:05:29 -0700

    Fixed stargazer table when knit

--- FINAL_PROJECT/scratch.R
+++ FINAL_PROJECT/scratch.R
@@ -39,26 +39,42 @@ coeftest(indiv_IncomePercentile_Age_at_Death_Model, vcov. = vcovHC, type = "HC1"
 
 
 
-# Plot Income and Mortality 
-plot(x = data_table_16$indv_inc, 
-     y = data_table_16$mortrate, 
-     xlab = "Income",
-     ylab = "Mortality Rate",
-     main = "Mortality Rates and Income",
-     col = "steelblue")
-abline(indiv_Income_Mortality_Model, col = "red")
+combined_life_expect <- lm(data = data_table_16, data_table_16$age_at_d ~ data_table_16$indv_pctile + data_table_16$indv_inc + data_table_16$gnd)
+summary(combined_life_expect)
+coeftest(combined_life_expect, vcov. = vcovHC, type = "HC1")
+
+combined_mortality <- lm(data = data_table_16, data_table_16$mortrate ~ data_table_16$indv_pctile + data_table_16$indv_inc + data_table_16$gnd)
+summary(combined_mortality)
+coeftest(combined_mortality, vcov. = vcovHC, type = "HC1")
+
+
+standard_err <- list(sqrt(diag(vcovHC(combined_life_expect, combined_mortality, type = "HC1"))))
+
+
+# STARGAZER TABLES
+stargazer(combined_life_expect, combined_mortality,
+          digits = 3,
+          header = FALSE,
+          se = standard_err,
+          type = "latex", 
+          title = "combined life expectancy model",
+          model.numbers = FALSE,
+          out="combined_model.txt")
 
 
 
 # gather clustered standard errors in a list
 clustered_standard_errors <- list(sqrt(diag(vcovHC(indiv_Income_Mortality_Model, type = "HC1"))),
+                                  
+               sqrt(diag(vcovHC(gender, type = "HC1"))),
+               
                sqrt(diag(vcovHC(indiv_Income_Age_at_Death_Model, type = "HC1"))),
                sqrt(diag(vcovHC(indiv_Income_pctile_Mortality_Model, type = "HC1"))),
                sqrt(diag(vcovHC(indiv_IncomePercentile_LifeExpectancy_Model, type = "HC1"))))
 
 
 # STARGAZER TABLES
-stargazer(indiv_Income_Mortality_Model, indiv_Income_Age_at_Death_Model, indiv_Income_pctile_Mortality_Model, indiv_IncomePercentile_LifeExpectancy_Model, 
+stargazer(indiv_Income_Mortality_Model, indiv_Income_Age_at_Death_Model, indiv_Income_pctile_Mortality_Model, indiv_IncomePercentile_LifeExpectancy_Model, gender, 
           digits = 3,
           header = FALSE,
           se = clustered_standard_errors,
@@ -66,4 +82,53 @@ stargazer(indiv_Income_Mortality_Model, indiv_Income_Age_at_Death_Model, indiv_I
           title = "Linear Panel Regression Models of Income and Life Expectancy",
           model.numbers = FALSE,
           out="stargazer_table.txt")
+
+
+
+# Plot Income and Mortality 
+plot(x = data_table_16$indv_inc, 
+     y = data_table_16$mortrate, 
+     xlab = "Income",
+     ylab = "Mortality Rate",
+     main = "Mortality Rates and Income",
+     col = "steelblue")
+# Add trend line
+abline(indiv_Income_Mortality_Model, col = "red")
+
+
+
+# Plot Income and Life Expectancy
+plot(x = data_table_16$indv_inc, 
+     y = data_table_16$mortrate, 
+     xlab = "Income",
+     ylab = "Mortality Rate",
+     main = "Mortality Rates and Income",
+     col = "steelblue")
+# Add trend line
+abline(indiv_Income_Mortality_Model, col = "red")
+
+
+
+# Plot Income Percentile and Mortality 
+plot(x = data_table_16$indv_inc, 
+     y = data_table_16$mortrate, 
+     xlab = "Income",
+     ylab = "Mortality Rate",
+     main = "Mortality Rates and Income",
+     col = "steelblue")
+# Add trend line
+abline(indiv_Income_Mortality_Model, col = "red")
+
+
+
+# Plot Income Percentile and Life Expectancy 
+plot(x = data_table_16$indv_inc, 
+     y = data_table_16$mortrate, 
+     xlab = "Income",
+     ylab = "Mortality Rate",
+     main = "Mortality Rates and Income",
+     col = "steelblue")
+# Add trend line
+abline(indiv_Income_Mortality_Model, col = "red")
+
 
