03c4cba4ad1120e20892236771c27ac561a072ca
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-12-14 13:37:12 -0700

    New model specifications

--- FINAL_PROJECT/scratch.R
+++ FINAL_PROJECT/scratch.R
@@ -1,41 +1,24 @@
 # Code
 
 
-# Model Specifications:
+# Mortality Rate Model Specifications:
+Mortality.model.1 <- lm(data_table_16$mortrate ~ data_table_16$indv_inc)
 
-# sink(file = "model_summaries.txt")
+Mortality.model.2 <- lm(data_table_16$mortrate ~ data_table_16$indv_inc + data_table_16$gnd)
 
-# Individual Income and mortality rate
-indiv_Income_Mortality_Model <- lm(data_table_16$mortrate ~ data_table_16$indv_inc + data_table_16$gnd)
-cat("Individual Income and Mortality Rate Model:")
-cat("\n ---------------------------------------------------------------")
-summary(indiv_Income_Mortality_Model)
-coeftest(indiv_Income_Mortality_Model, vcov. = vcovHC, type = "HC1")
+Mortality.model.3 <- lm(data_table_16$mortrate ~ data_table_16$indv_inc + data_table_16$gnd + data_table_16$indv_pctile)
+
+Mortality.model.4 <- lm(data_table_16$mortrate ~ data_table_16$indv_inc + data_table_16$gnd + data_table_16$indv_pctile + data_table_16$age_at_d)
 
 
-# Individual Income and Age at Death
-indiv_Income_Age_at_Death_Model <- lm(data_table_16$age_at_d ~ data_table_16$indv_inc)
-cat("Individual Income and Age at Death Model:")
-cat("\n ---------------------------------------------------------------")
-summary(indiv_Income_Age_at_Death_Model)
-coeftest(indiv_Income_Age_at_Death_Model, vcov. = vcovHC, type = "HC1")
+# Life Expectancy (Age at Death) Model Specifications:
+Life.Expect.model.1 <- lm(data_table_16$age_at_d ~ data_table_16$indv_inc)
 
+Life.Expect.model.2 <- lm(data_table_16$age_at_d ~ data_table_16$indv_inc + data_table_16$gnd)
 
-# Individual Income Percentile and Mortality Rate Model
-indiv_Income_pctile_Mortality_Model <- lm(data_table_16$mortrate ~ data_table_16$indv_pctile)
-cat("Individual Income Percentile and Mortality Rate Model: \n ")
-summary(indiv_Income_pctile_Mortality_Model)
-coeftest(indiv_Income_pctile_Mortality_Model, vcov. = vcovHC, type = "HC1")
+Life.Expect.model.3 <- lm(data_table_16$age_at_d ~ data_table_16$indv_inc + data_table_16$gnd + data_table_16$indv_pctile)
 
-
-# Income Percentile and Age at Death
-indiv_IncomePercentile_Age_at_Death_Model <- lm(data_table_16$age_at_d ~ data_table_16$indv_pctile)
-cat("Individual Income Percentile and Age at Death Model:")
-cat("\n ---------------------------------------------------------------")
-summary(indiv_IncomePercentile_Age_at_Death_Model)
-coeftest(indiv_IncomePercentile_Age_at_Death_Model, vcov. = vcovHC, type = "HC1")
-
-# sink()
+Life.Expect.model.4 <- lm(data_table_16$age_at_d ~ data_table_16$indv_inc + data_table_16$gnd + data_table_16$indv_pctile + data_table_16$mortrate)
 
 
 
@@ -48,42 +31,34 @@ summary(combined_mortality)
 coeftest(combined_mortality, vcov. = vcovHC, type = "HC1")
 
 
-standard_err <- list(sqrt(diag(vcovHC(combined_life_expect, combined_mortality, type = "HC1"))))
+
+# gather clustered standard errors in a list
+standard_errors <- list(sqrt(diag(vcovHC(Mortality.model.1, type = "HC1"))),
+               sqrt(diag(vcovHC(Mortality.model.2, type = "HC1"))),
+               sqrt(diag(vcovHC(Mortality.model.3, type = "HC1"))),
+               sqrt(diag(vcovHC(Mortality.model.4, type = "HC1"))),
+               
+               sqrt(diag(vcovHC(Life.Expect.model.1, type = "HC1"))),
+               sqrt(diag(vcovHC(Life.Expect.model.2, type = "HC1"))),
+               sqrt(diag(vcovHC(Life.Expect.model.3, type = "HC1"))),
+               sqrt(diag(vcovHC(Life.Expect.model.4, type = "HC1"))))
 
 
 # STARGAZER TABLES
-stargazer(combined_life_expect, combined_mortality,
+stargazer(Mortality.model.1, Mortality.model.2, Mortality.model.3, Mortality.model.4, Life.Expect.model.1, Life.Expect.model.2, Life.Expect.model.3, Life.Expect.model.4,
           digits = 3,
           header = FALSE,
           se = standard_err,
           type = "latex", 
           title = "combined life expectancy model",
           model.numbers = FALSE,
-          out="combined_model.txt")
+          out="new_models.txt")
 
 
 
-# gather clustered standard errors in a list
-clustered_standard_errors <- list(sqrt(diag(vcovHC(indiv_Income_Mortality_Model, type = "HC1"))),
-                                  
-               sqrt(diag(vcovHC(gender, type = "HC1"))),
-               
-               sqrt(diag(vcovHC(indiv_Income_Age_at_Death_Model, type = "HC1"))),
-               sqrt(diag(vcovHC(indiv_Income_pctile_Mortality_Model, type = "HC1"))),
-               sqrt(diag(vcovHC(indiv_IncomePercentile_LifeExpectancy_Model, type = "HC1"))))
 
 
-# STARGAZER TABLES
-stargazer(indiv_Income_Mortality_Model, indiv_Income_Age_at_Death_Model, indiv_Income_pctile_Mortality_Model, indiv_IncomePercentile_LifeExpectancy_Model, gender, 
-          digits = 3,
-          header = FALSE,
-          se = clustered_standard_errors,
-          type = "latex", 
-          title = "Linear Panel Regression Models of Income and Life Expectancy",
-          model.numbers = FALSE,
-          out="stargazer_table.txt")
-
-
+# Plots and Graphs
 
 # Plot Income and Mortality 
 plot(x = data_table_16$indv_inc, 
